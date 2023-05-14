f9d2fd4c175bb6ed0c219844ad25f7884e99937c
1234Sumuu <41938215+1234Sumuu@users.noreply.github.com>
2023-05-14 10:48:04 +0600

    person.js

--- person.js
+++ person.js
@@ -1,1 +1,14 @@
+const person = {
+  name = 'Sumaiya',
+  walk(){
+//   talk();
+  console.log(this);
+  }
+};
+// person.talk();
+person['name'] = 'ELSA';
+//Binding this
 
+
+
+//this in js alwayse return a reference of a current object.
